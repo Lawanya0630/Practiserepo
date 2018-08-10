5cecb4c2e7c7313ac65039c795666b842e122035
Lawanya V <CIPL0630@colanonline.local>
2018-08-10 13:17:11 +0530

    Changing data

---
+++ Pull_command/Java/sample.java
@@ -0,0 +1,10 @@
+class sample
+{
+public static void main(String arg[])
+{
+System.out.println("Hello World");
+System.out.println("Hi! I added one"); 
+System.out.println("Hi! I added two"); 
+System.out.println("Hi! I added three"); 
+}
+}
---
+++ Pull_command/Java/second.java
@@ -0,0 +1,10 @@
+class second
+{
+public static void main(String arg[])
+{
+int a=5,b=5;
+int c=a+b;
+System.out.println("C value is : "+c);
+System.out.println("Second");
+}
+}
